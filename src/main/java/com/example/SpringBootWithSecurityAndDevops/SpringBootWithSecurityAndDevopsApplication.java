007b9290ad4b1108e661d6021b1261b874b08164
mohibul361 <mohibul323@gmail.com>
2025-05-19 15:58:59 +0600

    Initial commit - add Spring Boot project

---
+++ src/main/java/com/example/SpringBootWithSecurityAndDevops/SpringBootWithSecurityAndDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootWithSecurityAndDevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithSecurityAndDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithSecurityAndDevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootWithSecurityAndDevops/SpringBootWithSecurityAndDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootWithSecurityAndDevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithSecurityAndDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
